d6dfbef83688819c489e187301efc9af819223ef
deepankarvarma <satwikdpshrit@gmail.com>
2023-06-11 11:21:27 +0530

    working code

--- app.py
+++ app.py
@@ -1,58 +1,29 @@
 import streamlit as st
 import cv2
 import numpy as np
-import tempfile
-import os
+import rembg
 
-def remove_background(image_path):
-    # Load the image
-    image = cv2.imread(image_path)
-
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Apply thresholding to create a mask
-    _, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-    # Apply morphological operations to remove noise
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=3)
-    sure_bg = cv2.dilate(opening, kernel, iterations=3)
-
-    # Create a mask image with transparent background
-    transparent_mask = cv2.cvtColor(sure_bg, cv2.COLOR_GRAY2BGRA)
-    transparent_mask[:, :, 3] = 0
-
-    # Apply the mask to the original image
-    result = cv2.bitwise_and(image, image, mask=sure_bg)
-
-    # Convert the result to RGB
-    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
-
+def remove_background(image):
+    result = rembg.remove(image)
     return result
 
-# Streamlit app
-st.title("Background Remover")
+def main():
+    st.title("Background Remover")
+    
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    
+    if uploaded_image is not None:
+        image = np.array(bytearray(uploaded_image.read()), dtype=np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        
+        st.subheader("Original Image")
+        st.image(image, channels="RGB")
+        
+        removed_background = remove_background(image)
+        
+        st.subheader("Background Removed")
+        st.image(removed_background, channels="RGBA")
+    
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    # Save the uploaded file to a temporary directory
-    temp_dir = tempfile.mkdtemp()
-    temp_path = os.path.join(temp_dir, uploaded_file.name)
-    with open(temp_path, 'wb') as f:
-        f.write(uploaded_file.read())
-
-    # Display the uploaded image
-    image = cv2.imread(temp_path)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-
-    # Remove the background
-    result = remove_background(temp_path)
-
-    # Display the result
-    st.image(result, caption='Background Removed', use_column_width=True)
-
-    # Clean up the temporary directory
-    os.remove(temp_path)
-    os.rmdir(temp_dir)
+if __name__ == "__main__":
+    main()
